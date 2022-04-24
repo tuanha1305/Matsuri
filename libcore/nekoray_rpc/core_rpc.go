92e65c1123b289ed416e778604d1c95a0f7a14e7
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2022-04-24 22:45:23 +0800

    nekoray: speedtest

--- libcore/nekoray_rpc/core_rpc.go
+++ libcore/nekoray_rpc/core_rpc.go
@@ -17,12 +17,15 @@ func setupCore() {
 	libcore.InitCore("", "", "", nil, ".", "moe.nekoray.pc:bg", true, 50*1024)
 }
 
-func (s *server) Start(ctx context.Context, in *LoadConfigReq) (resp *ErrorResp, _ error) {
+func (s *server) Start(ctx context.Context, in *LoadConfigReq) (out *ErrorResp, _ error) {
 	var err error
+
+	// only error use this
 	defer func() {
-		resp = &ErrorResp{}
+		out = &ErrorResp{}
 		if err != nil {
-			resp.Error = err.Error()
+			out.Error = err.Error()
+			instance = nil
 		}
 	}()
 
@@ -50,6 +53,8 @@ func (s *server) Start(ctx context.Context, in *LoadConfigReq) (resp *ErrorResp,
 
 func (s *server) Stop(ctx context.Context, in *EmptyReq) (resp *ErrorResp, _ error) {
 	var err error
+
+	// only error use this
 	defer func() {
 		resp = &ErrorResp{}
 		if err != nil {
@@ -65,17 +70,41 @@ func (s *server) Stop(ctx context.Context, in *EmptyReq) (resp *ErrorResp, _ err
 	return
 }
 
-func (s *server) Exit(ctx context.Context, in *EmptyReq) (resp *ErrorResp, _ error) {
-	var err error
-	defer func() {
-		resp = &ErrorResp{}
-		if err != nil {
-			resp.Error = err.Error()
-		}
-	}()
+func (s *server) Exit(ctx context.Context, in *EmptyReq) (out *EmptyResp, _ error) {
+	out = &EmptyResp{}
 
 	// Connection closed
 	os.Exit(0)
+	return
+}
+
+func (s *server) Test(ctx context.Context, in *TestReq) (out *TestResp, _ error) {
+	var err error
+	out = &TestResp{Ms: 0}
+
+	defer func() {
+		if err != nil {
+			out.Error = err.Error()
+		}
+	}()
+
+	if in.Mode == TestMode_UrlTest {
+		i := libcore.NewV2rayInstance()
+		err = i.LoadConfig(in.Config.CoreConfig)
+		if err != nil {
+			return
+		}
+		err = i.Start()
+		if err != nil {
+			return
+		}
+		var t int32
+		t, err = libcore.UrlTestV2ray(i, in.Inbound, in.Url, in.Timeout)
+		out.Ms = t // sn: ms==0 是错误
+		i.Close()
+	} else {
+		// TODO  impl
+	}
 
 	return
 }
